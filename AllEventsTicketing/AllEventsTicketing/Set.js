82c0cfd225bc378f74ae1bb14b16089b7950b45c
Josh <jfallin92@gmail.com>
2018-05-01 20:50:04 -0600

    initial commit with Assign01

---
+++ AllEventsTicketing/AllEventsTicketing/Set.js
@@ -0,0 +1,68 @@
+function Set() {
+	
+	
+	this.intersection = function(listA, listB) {
+    
+	   var resultList = [];
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   if (listA === null || listB === null) {
+	   	return null;
+	   }
+
+	   for (var i = 0; i < listA.length; i++) {
+	   	 var nextValue = listA[i];
+	   	 for (var j = 0; j < listA.length; j++) {
+	   		if (listB[j] === nextValue) {
+                resultList.push(listB[j]);
+                break;
+			}
+		}
+	   }
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}
+    
+    
+    
+	this.union = function(listA, listB) {
+
+	   var resultList = [];
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   
+	   /*-------------------------------Insert your code here -------------------------------------*/ 
+	   
+	   return resultList;
+	}
+
+
+
+
+	this.relativeComplement = function(listA, listB) {
+
+	   var resultList = [];
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   	   
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}
+
+
+
+	this.symmetricDifference = function(listA, listB) {
+
+	   var resultList = [];
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}	
+	
+
+}
